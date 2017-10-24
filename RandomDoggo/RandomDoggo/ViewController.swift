5ec0207835e7ce5bcc94edee6344fee3f433c2f1
tsukudabuddha <andrewtsukuda@gmail.com>
2017-10-24 16:27:20 -0700

    first commit

---
+++ RandomDoggo/RandomDoggo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomDoggo
+//
+//  Created by Andrew Tsukuda on 10/24/17.
+//  Copyright © 2017 Andrew Tsukuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
